f6aefe0c49e1bcbdd38103381573c304f9913a74
Vavashka23 <32300669+Vavashka23@users.noreply.github.com>
2018-05-05 16:28:15 +0300

    adding scalaTests

--- src/main/Fifth.scala
+++ src/main/Fifth.scala
@@ -5,14 +5,14 @@ import scala.collection.mutable
 
 object Fifth{
   def main(args: Array[String]): Unit = {
-    val exponent = 4;
-    val number = 5;
+    val exponent = 4
+    val number = 5
     val list1 = mutable.MutableList[Int](1,0,2,0,-1,-2,1,-5,6,-4)
     val list2 = mutable.MutableList[Int]()
     println(exponentRecursive(number,exponent))
     println(exponentTailRecursive(number, exponent))
-    println(bigNumNotRec(list1, list2))
-    println(bigNumTailRec(list1))
+    println(lowNumNotRec(list1, list2))
+    println(lowNumTailRec(list1))
 
   }
 
@@ -32,14 +32,14 @@ object Fifth{
     expIter(number, exponent, 0 , 1)
   }
 
-  def bigNumNotRec(oldList: mutable.MutableList[Int], newList: mutable.MutableList[Int]): mutable.MutableList[Int] = {
-    for(i <- 2 to oldList.size - 1){
+  def lowNumNotRec(oldList: mutable.MutableList[Int], newList: mutable.MutableList[Int]): mutable.MutableList[Int] = {
+    for(i <- 2 until oldList.size){
       if(oldList(i) < (oldList(i - 2) - oldList(i - 1))) newList += oldList(i)
     }
     newList
   }
 
-  def bigNumTailRec(oldList: mutable.MutableList[Int]): mutable.MutableList[Int] = {
+  def lowNumTailRec(oldList: mutable.MutableList[Int]): mutable.MutableList[Int] = {
     @tailrec
     def find(oldList: mutable.MutableList[Int], newList: mutable.MutableList[Int], index: Int): mutable.MutableList[Int] = {
       if (index < oldList.size) {

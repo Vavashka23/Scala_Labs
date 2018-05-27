78754a1fa01c4464ff0052b1b9c44c384b20c5ec
Vavashka23 <32300669+Vavashka23@users.noreply.github.com>
2018-05-27 17:47:35 +0300

    working 7 lab

--- src/main/Seven.scala
+++ src/main/Seven.scala
@@ -5,11 +5,45 @@ object Seven {
   def main(args: Array[String]): Unit = {
     val myColor = Tuple3(30, 128, 255)
     println(myConvert(myColor))
+    val rec = Rectangle(12,13)
+    val parallelepiped = Parallelepiped(10,11,4)
+    chooseFigure(rec)
+    chooseFigure(parallelepiped)
   }
 
   def myConvert(old: (Int, Int, Int)): (Int, Int, Int) = {
     val result: Int = (old._1+old._2+old._3)/3+1
     val newColor = Tuple3(result, result, result)
     newColor
+  }
+
+  def chooseFigure(obj: AnyRef)=
+    obj match {
+      case obj:Rectangle => sFig(obj.unapply(obj))
+      case obj:Parallelepiped => sFig(obj.unapply(obj))
+      case _ => println("nothing")
+    }
+
+  case class Rectangle(a:Int, b:Int) {
+    def unapply(arg: Rectangle): Option[(Int, Int, Int)] = {
+      Some(arg.a, arg.b, 0)
+    }
+  }
+
+  case class Parallelepiped(a:Int, b:Int, c:Int) {
+    def unapply(arg: Parallelepiped): Option[(Int, Int, Int)] = {
+      Some(arg.a, arg.b, arg.c)
+    }
+  }
+
+  def sFig(param: Option[(Int, Int, Int)]):Int ={
+    if(param.get._3 == 0)
+      {
+        val s: Int = param.get._1*param.get._2
+        s
+      } else {
+      val s: Int = 2 * (param.get._1 * param.get._2 + param.get._1 * param.get._3 + param.get._2 * param.get._3)
+      s
+    }
   }
 }
--- src/test/TestSeven.scala
+++ src/test/TestSeven.scala
@@ -8,6 +8,13 @@ class TestSeven extends FlatSpec with Matchers {
   it should "Color conversion: RGB to GrayScale" in {
     val myRGB = Tuple3(25,215,137)
     val newGray = Tuple3(126,126,126)
-    Seven.myConvert(myRGB) == newGray
+    assert(Seven.myConvert(myRGB) == newGray)
+  }
+
+  it should "Calculating area function" in {
+    val fig1 = Seven.Rectangle(12,2)
+    val fig2 = Seven.Parallelepiped(4,7,9)
+    assert(Seven.chooseFigure(fig1) == 24)
+    assert(Seven.chooseFigure(fig2) == 254)
   }
 }
